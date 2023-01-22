4058ae13fc0428d268b9d95186be7ad0c79336c1
Dani-Cano <dcanosaavedra@danielcastelao.org>
2023-01-22 20:58:54 +0100

    B

--- src/App/Aplicacion.java
+++ src/App/Aplicacion.java
@@ -1,6 +1,8 @@
 package App;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Aplicacion {
     private JPanel root;
@@ -25,4 +27,12 @@ public class Aplicacion {
     private JButton btnSuma;
     private JLabel lblDisplay;
 
+    public App() {
+        btnReset.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("");
+                lblDisplay.setText("");
+            }
+        });
 }
