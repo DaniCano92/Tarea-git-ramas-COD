69e31a41da39c0f07a7a663572b1a67857e0a0f3
Dani-Cano <dcanosaavedra@danielcastelao.org>
2023-01-22 21:11:04 +0100

    E

--- src/App/Aplicacion.java
+++ src/App/Aplicacion.java
@@ -1,4 +1,23 @@
 package App;
 
 public class Aplicacion {
+    public String calcular(String operacion) {
+        String resultado;
+        String[] ops = operacion.split( "[\\+\\-X\\/]");
+        if (ops.length == 1) {
+            resultado = ops[0];
+        } else {
+            float v1 = Float.parseFloat(ops[0]);
+            float v2 = Float.parseFloat(ops[1]);
+            float r;
+            if (operacion.contains("+"))
+                r = v1 + v2;
+            else if (operacion.contains("-"))
+                r = v1 - v2;
+            else if (operacion.contains("X"))
+                r = v1 * v2;
+            else r = v1 / v2;
+            resultado = String.valueOf(r);
+        }
+        return resultado;
 }
