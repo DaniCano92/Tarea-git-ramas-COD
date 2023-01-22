3d3e486cbfae5abc9694cc6a984c2ae078a1a2d5
Dani-Cano <dcanosaavedra@danielcastelao.org>
2023-01-22 20:56:47 +0100

    D

--- src/App/Aplicacion.java
+++ src/App/Aplicacion.java
@@ -1,4 +1,28 @@
 package App;
 
+import javax.swing.*;
+
 public class Aplicacion {
+    private JPanel root;
+    private JPanel Cabecera;
+    private JPanel Botonera;
+    private JButton btnReset;
+    private JButton a7Button;
+    private JButton a4Button;
+    private JButton a1Button;
+    private JButton btnPunto;
+    private JButton a8Button;
+    private JButton a9Button;
+    private JButton btnDivision;
+    private JButton a5Button;
+    private JButton a6Button;
+    private JButton btnMultiplicacion;
+    private JButton a2Button;
+    private JButton a3Button;
+    private JButton btnResta;
+    private JButton a0Button;
+    private JButton btnIgual;
+    private JButton btnSuma;
+    private JLabel lblDisplay;
+
 }
